85881fe208da0fe068d5d3eeeddad3d06314f182
lhj4499 <ucs210212@naver.com>
2022-10-23 16:03:45 +0900

    다이얼로그 추가

--- app/src/main/java/com/ssu/gardenmaker/ui/view/activity/MainActivity.kt
+++ app/src/main/java/com/ssu/gardenmaker/ui/view/activity/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.ssu.gardenmaker.ui.view.activity
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.ExpandableListView
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 import androidx.activity.viewModels
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
@@ -29,6 +30,85 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main), 
         binding.lifecycleOwner = this
 
         initNavigationMenu()
+        binding.mainToolbar.findViewById<ImageButton>(R.id.Plusplan_btn_main).setOnClickListener{
+            var CustomDialog= Dialog(this@MainActivity)
+            CustomDialog.setContentView(R.layout.makeplan_dialog)
+            CustomDialog.show()
+
+            var checkbox_Check=CustomDialog.findViewById<CheckBox>(R.id.Checkbox_Check)
+            var checkbox_Timer=CustomDialog.findViewById<CheckBox>(R.id.Checkbox_Timer)
+            var checkbox_Pedometer=CustomDialog.findViewById<CheckBox>(R.id.Checkbox_Pedometer)
+            var checkbox_Goalcounter=CustomDialog.findViewById<CheckBox>(R.id.Checkbox_Goalcounter)
+
+            var goalparameter_tv=CustomDialog.findViewById<TextView>(R.id.GoalParmeter_tv_Dialog)
+            var goalparameter_btn=CustomDialog.findViewById<Button>(R.id.GoalParameter_btn_Dialog)
+            var countparameter_tv=CustomDialog.findViewById<TextView>(R.id.CountParameter_tv_Dialog)
+            var countparameter_btn=CustomDialog.findViewById<Button>(R.id.CountParameter_btn_Dialog)
+
+            var goaltimer_tv=CustomDialog.findViewById<TextView>(R.id.GoalTimer_tv_Dialog)
+            var goaltimer_btn=CustomDialog.findViewById<Button>(R.id.GoalTimer_btn_Dialog)
+
+            var goalcounter_tv=CustomDialog.findViewById<TextView>(R.id.CountGoalcounter_tv_Dialog)
+            var goalcounter_btn=CustomDialog.findViewById<Button>(R.id.CountGoalcounter_btn_Dialog)
+
+            //문제: 모든 checkbox에 대해서 해지가 가능하다.
+            checkbox_Check.setOnCheckedChangeListener{ view, isChecked ->
+                if(isChecked){
+                    goalparameter_tv.visibility= View.GONE
+                    goalparameter_btn.visibility= View.GONE
+                    countparameter_tv.visibility= View.GONE
+                    countparameter_btn.visibility= View.GONE
+                    goaltimer_tv.visibility= View.GONE
+                    goaltimer_btn.visibility= View.GONE
+                    goalcounter_tv.visibility= View.GONE
+                    goalcounter_btn.visibility= View.GONE
+                }else{
+
+                }
+            }
+            checkbox_Timer.setOnCheckedChangeListener{ view, isChecked ->
+                if(isChecked){
+                    goalparameter_tv.visibility= View.GONE
+                    goalparameter_btn.visibility= View.GONE
+                    countparameter_tv.visibility= View.GONE
+                    countparameter_btn.visibility= View.GONE
+                    goaltimer_tv.visibility= View.VISIBLE
+                    goaltimer_btn.visibility= View.VISIBLE
+                    goalcounter_tv.visibility= View.GONE
+                    goalcounter_btn.visibility= View.GONE
+                }else{
+
+                }
+            }
+            checkbox_Pedometer.setOnCheckedChangeListener{ view, isChecked ->
+                if(isChecked){
+                    goalparameter_tv.visibility= View.VISIBLE
+                    goalparameter_btn.visibility= View.VISIBLE
+                    countparameter_tv.visibility= View.VISIBLE
+                    countparameter_btn.visibility= View.VISIBLE
+                    goaltimer_tv.visibility= View.GONE
+                    goaltimer_btn.visibility= View.GONE
+                    goalcounter_tv.visibility= View.GONE
+                    goalcounter_btn.visibility= View.GONE
+                }else{
+
+                }
+            }
+            checkbox_Goalcounter.setOnCheckedChangeListener{ view, isChecked ->
+                if(isChecked){
+                    goalparameter_tv.visibility= View.GONE
+                    goalparameter_btn.visibility= View.GONE
+                    countparameter_tv.visibility= View.GONE
+                    countparameter_btn.visibility= View.GONE
+                    goaltimer_tv.visibility= View.GONE
+                    goaltimer_btn.visibility= View.GONE
+                    goalcounter_tv.visibility= View.VISIBLE
+                    goalcounter_btn.visibility= View.VISIBLE
+                }else{
+
+                }
+            }
+        }
     }
 
     // 툴바 메뉴 버튼이 클릭 됐을 때 실행하는 함수
